404dec16f57e5e3e69969fbc3e46d22ada4e6853
Alex Kekee <kekekuza@gmail.com>
2023-03-28 05:22:37 -0700

    Hello world in C

---
+++ C-language/0x01-Introduction/HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function to print hello world
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Hello world! C programming is Cool\n");
+
+	return (0);
+}
