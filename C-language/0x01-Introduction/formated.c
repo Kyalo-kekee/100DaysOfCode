6536b0cc11f4f64e070d1cd5a309e6363b749f89
Alex Kekee <kekekuza@gmail.com>
2023-03-28 05:38:05 -0700

    formatted output using printf

---
+++ C-language/0x01-Introduction/formated.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- using printf for formatted output
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("My name is %s alias %s, Iam %d years\n", "Alex", "Isiekee", 20);
+	printf("My current networth is \'$ %4.2f \'", 23000.5);
+
+	return (0);
+}
